a77b4e93f3b2b0f8ef3b4012c9b1c87e87190391
Tanguy <35039373+tanguymartinez@users.noreply.github.com>
2018-06-19 15:37:05 +0200

    Create MessageRequest

--- app/Http/Controllers/MessagesController.php
+++ app/Http/Controllers/MessagesController.php
@@ -4,18 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Message;
+use App\Http\Requests\MessageRequest;
 
 
 class MessagesController extends Controller
 {
-    public function submit(Request $request){
-
-        $this->validate($request, [
-          'name' => 'required',
-          'email' => 'required',
-          'message' => 'required',
-        ]);
-
+    public function submit(MessageRequest $request){
         $message = new Message;
         $message->name = $request->input('name');
         $message->email = $request->input('email');

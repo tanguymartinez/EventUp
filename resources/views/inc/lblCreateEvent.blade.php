04ed1ecb1f36cee638081294d96c122c952d0c6d
Clément Coulon <cle.coulon@gmail.com>
2018-06-21 10:02:04 +0200

    route Creer event modif

--- resources/views/inc/lblCreateEvent.blade.php
+++ resources/views/inc/lblCreateEvent.blade.php
@@ -1,3 +1,9 @@
+@if(Auth::check())
 <a class="txtCreateEvent" href="/event/create">
   Créer un Event <button class="plus">+</button>
 </a>
+@else
+<a class="txtCreateEvent" href="/connexion">
+  Créer un Event <button class="plus">+</button>
+</a>
+@endif

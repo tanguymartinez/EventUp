58f247b3f0b328915b76355712e9e737bdfceaee
Clément Coulon <cle.coulon@gmail.com>
2018-06-22 13:39:28 +0200

    last correction

---
+++ resources/views/moncompte.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="contentAccountShow">
+
+      {{ csrf_field() }}
+      <h3>MON COMPTE</h3><br>
+
+       <p class="nameMA">Nom utilisateur : {{ Auth::user()->name }}</p> <br>
+       <p class="emailMA">Mon email : {{ Auth::user()->email }}</p> <br>
+
+      <button class="btnModifAccountMA" type="submit" name="btnModifAccountMA">Modifier les paramètres du compte</button>
+
+  </div>
+@endsection
